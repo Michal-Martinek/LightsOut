040e1528b6363af4b21e54c98d3748c352531dfa
Michal Martínek <michal7952@seznam.cz>
2022-05-26 19:06:29 +0200

    Added winning and restart

--- LightsOut.py
+++ LightsOut.py
@@ -1,18 +1,27 @@
 import pygame
 import random
 
-NODE_NUMBER = 4
+NODE_NUMBER = 3
 NODE_SIZE = 120
 NODE_SPACING = 25
 BLOCK_SIZE = NODE_SIZE + NODE_SPACING
+
 HEADER_HEIGHT = 50
-SCREEN_SIZE = NODE_NUMBER  * BLOCK_SIZE + NODE_SPACING
+SCREEN_WIDTH = NODE_NUMBER  * BLOCK_SIZE + NODE_SPACING
+SCREEN_HEIGHT = NODE_NUMBER  * BLOCK_SIZE + NODE_SPACING + HEADER_HEIGHT
+
+SMILEY_POS = ((SCREEN_WIDTH - 40) // 2, (HEADER_HEIGHT - 40) // 2)
 
 # assets
 IMAGE_BULB_LIT = pygame.image.load('Assets\\BulbLit.png')
 IMAGE_BULB_LIT.set_colorkey((255, 255, 255))
 IMAGE_BULB_DARK = pygame.image.load('Assets\\BulbDark.png')
 IMAGE_BULB_DARK.set_colorkey((255, 255, 255))
+
+IMAGE_WIN = pygame.image.load('Assets\\FaceWin.png')
+IMAGE_WIN.set_colorkey((255, 255, 255))
+IMAGE_GAME = pygame.image.load('Assets\\FaceGame.png')
+IMAGE_GAME.set_colorkey((255, 255, 255))
 
 
 class LightsGrid:
@@ -26,7 +35,7 @@ class LightsGrid:
                                 self.neighborRules[y][x].append( (neighborX, neighborY) )
 
     def generateGrid(self):
-        numMoves = random.randint(1, self.size**2)
+        numMoves = random.randint(2, self.size**2-1)
         bulbs = []
         for x in range(self.size):
             bulbs.extend( [(x, y) for y in range(self.size)] )
@@ -49,21 +58,26 @@ class LightsGrid:
     def toggleAt(self, pos):
         for x, y in self.neighborRules[pos[1]][pos[0]]:
             self.grid[y][x] = not self.grid[y][x]
-    def click(self, pos):
-        pos = list(pos)
-        pos[1] -= HEADER_HEIGHT
-        if pos[1] < 0: return 
+    def click(self, pos) -> bool:
+        pos = [pos[0], pos[1] - HEADER_HEIGHT]
+        if pos[1] < 0: return False
         clicked = pos[0] // BLOCK_SIZE, pos[1] // BLOCK_SIZE
         remaining = pos[0] % BLOCK_SIZE, pos[1] % BLOCK_SIZE
         if (remaining[0] >= NODE_SPACING) and (remaining[1] >= NODE_SPACING):
             if (clicked[0] < self.size and clicked[1] < self.size):
                 self.toggleAt(clicked)
+        return all([all(row) for row in self.grid])
+
+def smileyClicked(pos) -> bool:
+    return SMILEY_POS[0] <= pos[0] <= SMILEY_POS[0]+40 and SMILEY_POS[1] <= pos[1] <= SMILEY_POS[1]+40
 
 def main():
-    display = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE + HEADER_HEIGHT))
+    display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Lights Out')
     grid = LightsGrid(NODE_NUMBER)
     clock = pygame.time.Clock()
+    gameWon = False
+    smileyFirstClicked = False
 
     running = True
     while running:
@@ -71,11 +85,22 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = event.pos
-                grid.click(pos)
+                if not gameWon:
+                    gameWon = grid.click(event.pos)
+                if smileyClicked(event.pos):
+                    if smileyFirstClicked:
+                        grid = LightsGrid(NODE_NUMBER)
+                        gameWon = False
+                        smileyFirstClicked = False
+                    else: 
+                        smileyFirstClicked = True
+
         display.fill((255, 255, 255))
         grid.drawGrid(display)
-        pygame.draw.rect(display, (0, 0, 255), (0, 0, SCREEN_SIZE, HEADER_HEIGHT))
+
+        pygame.draw.rect(display, (0, 0, 255), (0, 0, SCREEN_WIDTH, HEADER_HEIGHT))
+        display.blit((IMAGE_WIN if gameWon else IMAGE_GAME), SMILEY_POS)
+
         pygame.display.update()
         clock.tick(30)
     pygame.quit()
