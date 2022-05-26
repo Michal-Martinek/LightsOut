038b111ef5427a6a8f75c0fade85898659eeaae4
Michal Martínek <michal7952@seznam.cz>
2022-05-26 17:17:12 +0200

    Now generating solvable patterns

--- LightsOut.py
+++ LightsOut.py
@@ -17,24 +17,36 @@ class LightsGrid:
                             if (x == neighborX and y == neighborY) or random.random() >= 0.5:
                                 self.neighborRules[y][x].append( (neighborX, neighborY) )
 
+    def generateGrid(self):
+        numMoves = random.randint(1, self.size**2)
+        bulbs = []
+        for x in range(self.size):
+            bulbs.extend( [(x, y) for y in range(self.size)] )
+        moves = random.sample(bulbs, numMoves)
+        print('numMoves', numMoves, 'moves:', moves)
+        for move in moves:
+            self.toggleAt(move)
     def __init__(self, size: int):
         self.size = size
-        self.grid = [[(random.random() < 0.5) for x in range(size)] for y in range(size)]
+        self.grid = [[True for x in range(size)] for y in range(size)]
         self.neighborRules = [[[] for x in range(size)] for y in range(size)]
         self.generateNeighborRules()
+        self.generateGrid()
         
     def drawGrid(self, display):
         for y, row in enumerate(self.grid):
             for x, node in enumerate(row):
                 color = (255, 255, 0) if node else (0, 0, 255)
                 pygame.draw.rect(display, color, (x * BLOCK_SIZE + NODE_SPACING, y * BLOCK_SIZE + NODE_SPACING, NODE_SIZE, NODE_SIZE))
+    def toggleAt(self, pos):
+        for x, y in self.neighborRules[pos[1]][pos[0]]:
+            self.grid[y][x] = not self.grid[y][x]
     def click(self, pos):
         clicked = pos[0] // BLOCK_SIZE, pos[1] // BLOCK_SIZE
         remaining = pos[0] % BLOCK_SIZE, pos[1] % BLOCK_SIZE
         if (remaining[0] >= NODE_SPACING) and (remaining[1] >= NODE_SPACING):
             if (clicked[0] < self.size and clicked[1] < self.size):
-                for x, y in self.neighborRules[clicked[1]][clicked[0]]:
-                    self.grid[y][x] = not self.grid[y][x]
+                self.toggleAt(clicked)
 
 def main():
     display = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
